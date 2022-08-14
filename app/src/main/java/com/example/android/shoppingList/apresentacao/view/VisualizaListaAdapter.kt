a2c455c7c6d199f9c39c3e8c17f5e4ff925ad584
gabriel <gabrielvieiratobias@gmail.com>
2022-08-13 21:36:01 -0300

    - Implementando VisualizaListaAdapter

---
+++ app/src/main/java/com/example/android/shoppingList/apresentacao/view/VisualizaListaAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.android.shoppingList.apresentacao.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.shoppingList.R
+import com.example.android.shoppingList.apresentacao.model.ItensLista
+import com.example.android.shoppingList.apresentacao.model.Produto
+
+class VisualizaListaAdapter(): ListAdapter<ItensLista, VisualizaListaAdapter.ItensViewHolder>(ITENS_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VisualizaListaAdapter.ItensViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item, parent, false)
+        return VisualizaListaAdapter.ItensViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: VisualizaListaAdapter.ItensViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.NomeProduto)
+    }
+
+
+    class ItensViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val itensItemView: TextView = itemView.findViewById(R.id.txtViewNomeItem)
+
+        fun bind(text: String?) {
+            itensItemView.text = text
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): ItensViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_seleciona_produto, parent, false)
+
+                return ItensViewHolder(view)
+            }
+        }
+    }
+
+    companion object {
+        private val ITENS_COMPARATOR = object : DiffUtil.ItemCallback<ItensLista>() {
+            override fun areItemsTheSame(oldItem: ItensLista, newItem: ItensLista): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: ItensLista, newItem: ItensLista): Boolean {
+                return oldItem.NomeProduto == newItem.NomeProduto
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/android/shoppingList/apresentacao/viewModel/VisualizaListaViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.android.shoppingList.apresentacao.viewModel
+
+import androidx.lifecycle.*
+import com.example.android.shoppingList.apresentacao.ItensViewModel
+import com.example.android.shoppingList.apresentacao.model.ItensLista
+import com.example.android.shoppingList.apresentacao.model.Produto
+import com.example.android.shoppingList.dados.ItensListaRepository
+import com.example.android.shoppingList.dados.ProdutosRepository
+import kotlinx.coroutines.launch
+
+class VisualizaListaViewModel(private val repository: ItensListaRepository): ViewModel(){
+
+    val todosOsItens: LiveData<List<Produto>> = repository.retornaItensLista!!.asLiveData()
+
+
+    /**
+     * Usando coroutine para inserir os dados
+     */
+    fun insert(itensLista: ItensLista) = viewModelScope.launch {
+        repository.insert(itensLista)
+    }
+}
+
+class VisualizaItensViewModelFactory(private val repository: ItensListaRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(VisualizaListaViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return VisualizaListaViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Classe ViewModel não existe")
+    }
+}
